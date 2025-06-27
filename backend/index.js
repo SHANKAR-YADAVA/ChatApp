08b356b856fbf85a6a9c2517a2fa7f27065a7c8c
SHANKAR-YADAVA <shankaryadavar2004@gmail.com>
2025-06-28 01:59:06 +0530

    express is downgrade to suppot deployment

--- backend/index.js
+++ backend/index.js
@@ -32,12 +32,11 @@ app.use("/api/groups", groupRoutes);
 app.use("/api/translate", translateRoutes);
 
 
-// For serving frontend in production
 if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
