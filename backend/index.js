e50b7b69c29d5262e76d40234255926390790f86
SHANKAR-YADAVA <shankaryadavar2004@gmail.com>
2025-06-28 13:30:50 +0530

    solved regex issue

--- backend/index.js
+++ backend/index.js
@@ -31,13 +31,14 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/groups", groupRoutes);
 app.use("/api/translate", translateRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+ // Only match valid frontend routes, not malformed paths
+app.get(/^\/(?!api|.*:).*$/, (_, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
